747cb16ac5ff1e321c7be7f95ebbbdda33fd6d18
chhoung <phengchhoung@gmail.com>
2020-10-14 16:50:01 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/kittiepie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kittiepie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                  "https://purr.objects-us-east-1.dream.io/i/G4Iu1.jpg"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
