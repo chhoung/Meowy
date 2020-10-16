d1cd9be099a4a2396959ca2efac96c813280228e
chhoung <phengchhoung@gmail.com>
2020-10-16 15:19:03 +0700

    implemented api call

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() {
   runApp(MyApp());
@@ -9,6 +11,22 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+Future<String> getMeow() async {
+  var url = 'https://aws.random.cat/meow';
+  var response = await http.get(url);
+  var imageUrl;
+
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    imageUrl = jsonResponse['file'];
+
+    print(imageUrl);
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
+  return imageUrl;
+}
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
@@ -17,13 +35,51 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network(
-                  "https://purr.objects-us-east-1.dream.io/i/G4Iu1.jpg"),
-            ],
-          ),
+          child: FutureBuilder(
+              future: getMeow(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null) {
+                  return Center(
+                    child: Text('Loading...'),
+                  );
+                } else {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.network(
+                        snapshot.data,
+                        height: 300,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: RaisedButton(
+                              onPressed: () {
+                                setState(() {});
+                              },
+                              child: Text('Save'),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: RaisedButton(
+                              color: Colors.grey,
+                              textColor: Colors.black,
+                              onPressed: () {},
+                              child: Text('Share'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  );
+                }
+              }),
         ),
       ),
     );
